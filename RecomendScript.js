b086286e94708a1bf2827977f2705d333f1bedf0
Ashwin Chaurasiya <35067356+oaoashwin@users.noreply.github.com>
2022-12-05 22:36:45 +0530

    Fixed Style error and fixed a space

--- RecomendScript.js
+++ RecomendScript.js
@@ -2,14 +2,14 @@
 // of this website
 
 const header = document.querySelector('#primary-header')
-fetch('Sections/Header.html')
+fetch('../Sections/Header.html')
 .then(res=>res.text())
 .then(data=>{
     header.innerHTML=data
 })
 
 const footer = document.querySelector('#primary-footer')
-fetch('Sections/footer.html')
+fetch('../Sections/footer.html')
 .then(res=>res.text())
 .then(data=>{
     footer.innerHTML=data
